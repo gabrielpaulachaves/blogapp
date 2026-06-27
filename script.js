e850ddc629c209a4453657d56bbcddaaac60eae0
Gabriel <bielpchaves@gmail.com>
2026-06-27 13:03:24 -0300

    algumas anotacoes

--- script.js
+++ script.js
@@ -30,10 +30,16 @@ app.use(flash())
 //é importante que configurar o flash seja logo apos configurar a session
 
 //middleware
-app.use((req, res, next)=>{ //aqui vamos criar duas variaveis que poderemos acessar em qualquer parte da aplicacao, para criar, será res.locals."nome da variavel" = "valor"
+app.use((req, res, next)=>{ //aqui vamos criar duas variaveis que poderemos acessar em qualquer parte da aplicacao, para criar, será res.locals."nome da variavel" = "chave"
     res.locals.success_msg = req.flash("success_msg")
     res.locals.error_msg = req.flash("error_msg")
+    //o flash armazena valor temporario usando chave (a chave é a ("success_msg")), a variavel vai guardar a chave req.flash, e essa chave vai procurar o valor pelo codigo.
+    //Imagine a situação: req.flash("success_msg", "categoria criada"), entao o res.locals.success_msg vai procurar essa req.flash("success_msg") no codigo e seu valor será "categoria criada". Então para mostrarmos no handlebars, basta {{success_msg}}
+    //a chave é essa "success_msg" dentro do req.flash, ai quando achar um valor nessa chave, a variavel .locals. vai receber esse valor
     next()
+
+    //o middleware é como um intermediador que executa uma logica antes de dar a resposta de requisiçao ao cliente. Como por exemplo: uma tela de login antes de vc acessar o conteudo, se estiver logado, entao vai para a tela do conteudo, se nao estiver logado, vai pra tela de login.
+    //ele nao é apenas para esses casos, um body-parser tbm é um middleware
 })
 
 app.engine("handlebars", engine({defaultLayout:"main"}))
